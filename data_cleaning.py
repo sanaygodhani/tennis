52c417b99bca8330b6d5c0e6568080e39e489dbc
sanaygodhani <sanaygodhani@cmail.carleton.ca>
2025-06-30 22:10:59 -0400

    minor tweaks

--- data_cleaning.py
+++ data_cleaning.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import tqdm
+from tqdm import tqdm
 pd.set_option('display.max_columns', None)
 
 df = pd.read_csv("./data/singles_match_data/atp_matches_2024.csv")
